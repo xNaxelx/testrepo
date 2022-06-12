47a15b5bb419c9a8498b5b031c24a29769b2ee39
Naxel <46362288+xNaxelx@users.noreply.github.com>
2022-06-12 16:45:10 +0300

    Сделал ловушки
    И добавил пару методов в LootStorage и в Loot

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LootStorage _ls;
+    private uint victimNumberInStorage;
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if(collision.gameObject.GetComponent<Loot>() != null)
+        {
+            _ls = collision.gameObject.GetComponent<Loot>().hand.GetComponent<Hand>().lootStorage;
+            victimNumberInStorage = collision.gameObject.GetComponent<Loot>().numberInStorage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Destroy(_ls.GetLootGO(victimNumberInStorage));
+            _ls.LoseElement(victimNumberInStorage);
+
+            if(victimNumberInStorage != _ls.GetLastElementNumber())
+            {
+                for(uint v = victimNumberInStorage + 1, l = _ls.GetLastElementNumber(); v <= l; v++)
+                {
+                    _ls.GetLootGO(v).GetComponent<Loot>().TossTrap();
+                    _ls.LoseElement(v);
+                }                
+            }
+            _ls.CompressElements();
+        }
     }
 }
